21195968bc48efd75eb5d0adffc352d5283430d5
Derek Prieur <DPrieur123@gmail.com>
2023-11-17 10:14:48 -0500

    update

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,9 +1,11 @@
+import Sidebar from "@/components/dashboard/Sidebar";
+
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="mx-auto max-w-6xl px-4 pt-20 md:pt-24 2xl:max-w-screen-xl">
       <div className="flex gap-x-7">
         <div className="hidden w-64 shrink-0 md:block">
-          {/* TODO: Sidebar */}
+          <Sidebar />
         </div>
         {children}
       </div>
