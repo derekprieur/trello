16a20d718dccf9c0e303c54ab4bdde7261741d88
Derek Prieur <DPrieur123@gmail.com>
2023-11-17 09:58:16 -0500

    update

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/dashboard/Navbar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default DashboardLayout;
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,8 @@
+import { OrganizationSwitcher, auth } from "@clerk/nextjs";
+
 const OrganizationIdPage = () => {
-  return <div>OrganizationIdPage</div>;
+  const { userId, orgId } = auth();
+  return <div>Organization Page</div>;
 };
 
 export default OrganizationIdPage;
