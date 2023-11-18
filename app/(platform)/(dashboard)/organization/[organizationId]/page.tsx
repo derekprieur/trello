cd12a8be73d5c7af0f32645809654440726df40b
Derek Prieur <DPrieur123@gmail.com>
2023-11-18 12:52:49 -0500

    update

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,15 @@
+import BoardList from "@/components/organizationId/BoardList";
 import Info from "@/components/organizationId/Info";
+import { Separator } from "@/components/ui/separator";
 
 const OrganizationIdPage = async () => {
   return (
     <div className="mb-20 w-full">
       <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };
---
+++ components/Hint.tsx
@@ -0,0 +1,37 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+interface HintProps {
+  children: React.ReactNode;
+  description: string;
+  side?: "left" | "right" | "top" | "bottom";
+  sideOffset?: number;
+}
+
+const hint = ({
+  children,
+  description,
+  side = "bottom",
+  sideOffset = 0,
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          sideOffset={sideOffset}
+          side={side}
+          className="max-w-[220px] break-words text-xs"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
+
+export default hint;
