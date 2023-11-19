6ece040a7948d64776b14e408db608637b85bf64
Derek Prieur <DPrieur123@gmail.com>
2023-11-19 08:39:29 -0500

    update

--- components/boardId/CardForm.tsx
+++ components/boardId/CardForm.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { forwardRef } from "react";
+import { ElementRef, KeyboardEventHandler, forwardRef, useRef } from "react";
 import { Button } from "../ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
+import FormTextarea from "../form/FormTextarea";
+import { FormSubmit } from "../form/FormSubmit";
+import { useParams } from "next/navigation";
+import { useAction } from "@/hooks/useAction";
+import { createCard } from "@/actions/createCard";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import { toast } from "sonner";
 
 interface CardFormProps {
   listId: string;
@@ -13,6 +20,70 @@ interface CardFormProps {
 
 const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, enableEditing, disableEditing, isEditing }, ref) => {
+    const params = useParams();
+    const formRef = useRef<ElementRef<"form">>(null);
+    const { execute, fieldErrors } = useAction(createCard, {
+      onSuccess: (data: any) => {
+        toast.success(`Card "${data.title}" created!`);
+        formRef.current?.reset();
+      },
+      onError: (error: any) => {
+        toast.error(error);
+      },
+    });
+
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        disableEditing();
+      }
+    };
+
+    useOnClickOutside(formRef, disableEditing);
+
+    useEventListener("keydown", onKeyDown);
+
+    const onTextareaKeyDown: KeyboardEventHandler<HTMLTextAreaElement> = (
+      e
+    ) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        formRef.current?.requestSubmit();
+      }
+    };
+
+    const onSubmit = (formData: FormData) => {
+      const title = formData.get("title") as string;
+      const listId = formData.get("listId") as string;
+      const boardId = formData.get("boardId") as string;
+
+      execute({ title, listId, boardId });
+    };
+
+    if (isEditing) {
+      return (
+        <form
+          ref={formRef}
+          className="m-1 space-y-4 px-1 py-0.5"
+          action={onSubmit}
+        >
+          <FormTextarea
+            id="title"
+            onKeyDown={onTextareaKeyDown}
+            ref={ref}
+            placeholder="Enter a title for this card..."
+            errors={fieldErrors}
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add card</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+
     return (
       <div className="px-2 pt-2">
         <Button
--- components/form/FormTextarea.tsx
+++ components/form/FormTextarea.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import { KeyboardEventHandler, forwardRef } from "react";
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+import { cn } from "@/lib/utils";
+import FormErrors from "./FormErrors";
+import { useFormStatus } from "react-dom";
 
 interface FormTextareaProps {
   id: string;
@@ -33,7 +38,40 @@ const FormTextarea = forwardRef<HTMLTextAreaElement, FormTextareaProps>(
     },
     ref
   ) => {
-    return <div></div>;
+    const { pending } = useFormStatus();
+
+    return (
+      <div className="w-full space-y-2">
+        <div className="w-full space-y-1">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+          ) : null}
+          <Textarea
+            onKeyDown={onKeyDown}
+            onBlur={onBlur}
+            onClick={onClick}
+            ref={ref}
+            required={required}
+            placeholder={placeholder}
+            name={id}
+            id={id}
+            disabled={pending || disabled}
+            className={cn(
+              "resize-none focus-visible:ring-0 focus-visible:ring-offset-0 ring-0 focus:ring-0 outline-none shadow-sm",
+              className
+            )}
+            aria-describedby={`${id}-error`}
+            defaultValue={defaultValue}
+          />
+        </div>
+        <FormErrors id={id} errors={errors} />
+      </div>
+    );
   }
 );
 
