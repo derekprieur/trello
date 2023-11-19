7413a6f9eadc50f4b767091d86c99cf41cee328e
Derek Prieur <DPrieur123@gmail.com>
2023-11-19 09:40:12 -0500

    update

---
+++ components/modals/cardModal/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useCardModal } from "@/hooks/useCardModal";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+
+export const CardModal = () => {
+  const id = useCardModal((state) => state.id);
+  const isOpen = useCardModal((state) => state.isOpen);
+  const onClose = useCardModal((state) => state.onClose);
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6"></div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
